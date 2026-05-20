5afa1bf7a1434512339cc9d84b7893667bf1a574
aryanntyagi <ch1200078@iitd.ac.in>
2026-05-20 15:41:02 +0530

    Create Input-in-java.java

---
+++ Input-in-java.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Geeks
+{
+    public static void main(String[] args)
+    {
+    Scanner scn = new Scanner(System.in);
+    int a = scn.nextInt();
+    int b = scn.nextInt();
+    
+    System.out.println("The summation is :"  +  (a+b));
+    scn.close();
+    
+}
+}
+
+
